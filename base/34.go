c26eb9bdbe60f4b8c6c2471c1a82056a3eb92aef
berlin <bg226316@gmail.com>
2024-05-14 23:23:01 +0800

    feat(problem34):二分查找框架结构模板

---
+++ base/34.go
@@ -0,0 +1,35 @@
+package base
+
+func searchRange(nums []int, target int) []int {
+	left1, left2, right1, right2 := 0, 0, len(nums), len(nums)
+	for left1 < right1 || left2 < right2 {
+		if left1 < right1 {
+			mid1 := left1 + (right1 - left1)/2
+			if nums[mid1] == target {
+				right1 = mid1
+			}else if nums[mid1] < target {
+				left1 = mid1 + 1
+			}else if nums[mid1] > target {
+				right1 = mid1
+			}
+		}
+		if left2 < right2 {
+			mid2 := left2 + (right2 - left2)/2
+			if nums[mid2] == target {
+				left2 = mid2 + 1
+			}else if nums[mid2] < target {
+				left2 = mid2 + 1
+			}else if nums[mid2] > target {
+				right2 = mid2
+			}
+		}
+	}
+	if left1 >= len(nums) || left2 - 1 >= len(nums) {
+		return []int{-1, -1}
+	}
+	if nums[left1] == target && nums[left2 - 1] == target {
+		return  []int{left1, left2 - 1}
+	}else {
+		return []int{-1, -1}
+	}
+}
--- 代码框架/二分查找.go
+++ 代码框架/二分查找.go
@@ -2,6 +2,7 @@ package 代码框架
 
 //二分查找的思路很简单，但是边界问题很细致
 //计算时使用left + (right - left) / 2,有效防止了left 和 right太大,直接相加导致溢出的情况
+//不管是左闭右开还是左闭右闭,当一次查找之后,nums[mid]不是target值,之后就要根据区间来排除已经判断过不是的元素.
 
 func binarysearch(nums []int, target int) int {
 	low, high := 0, len(nums) - 1
@@ -16,4 +17,5 @@ func binarysearch(nums []int, target int) int {
 		}
 	}
 	return -1
+//	return nums[left] == target ? left : -1
 }
