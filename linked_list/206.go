00995442c5d80d6b16b0008ab540f64aac9d5c65
berlin <bg226316@gmail.com>
2024-05-23 21:46:18 +0800

    feat(problem206):数组|矩阵

---
+++ linked_list/206.go
@@ -0,0 +1,13 @@
+package linked_list
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
